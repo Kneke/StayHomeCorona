e493983d5505c4bc4c6c81ea03e3939b51d7a12b
JakobTeske <teske.jakob@icloud.com>
2020-03-21 17:13:40 +0100

    ompleted Challenges Endpoint and Repository
    Added env config

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"main/model"
 	"main/persistence"
 
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.POST("/challenges", createChallengeHandler)
 	r.GET("/challenges", challengeHandler)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -21,5 +23,23 @@ func challengeHandler(c *gin.Context) {
 	challenges := persistence.GetChallenges()
 	c.JSON(200, gin.H{
 		"values": challenges,
+	})
+}
+
+func createChallengeHandler(c *gin.Context) {
+	var challenge model.Challenge
+	if c.BindJSON(&challenge) == nil {
+		result, err := persistence.CreateChallenge(challenge)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err,
+			})
+		}
+		c.JSON(200, gin.H{
+			"result": result,
+		})
+	}
+	c.JSON(500, gin.H{
+		"error": "Bad Request, could not marshal JSON to Object",
 	})
 }
