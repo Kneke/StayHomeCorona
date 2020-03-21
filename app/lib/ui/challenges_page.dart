4ae3a557c5c9db9d31a3ff5a5121096d98842cdf
Christoph Knetschke <christoph.knetschke@daimler.com>
2020-03-21 15:02:44 +0100

    Add Drawer to App with navigation to different pages

---
+++ app/lib/ui/challenges_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Challenge Page')
+      ),
+    );
+  }
+}
