cd608804ee6fc5681dfc2846780b5f2f2bc3754e
Hannes Wagner <hannes_wagner@icloud.com>
2020-03-22 15:50:49 +0100

    adjust mainactivity.kt

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,25 +1,12 @@
 package com.example.app
 
-/*
 import android.support.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
-}*/
-
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterActivity() {
-    @java.lang.Override
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
     }
 }
